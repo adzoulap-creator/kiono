f4b099d85e15c18107b358c2b14eaa42911b7d77
adzoulap-creator <adzoulap@gmail.com>
2025-10-21 13:49:15 +0200

    description de ta mise à jour

--- static/pwabuilder-sw.js
+++ static/pwabuilder-sw.js
@@ -2,10 +2,8 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const CACHE = "pwabuilder-page";
-
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
+const CACHE = "pwabuilder-page-v1";
+const offlineFallbackPage = "index.html";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -16,7 +14,16 @@ self.addEventListener("message", (event) => {
 self.addEventListener('install', async (event) => {
   event.waitUntil(
     caches.open(CACHE)
-      .then((cache) => cache.add(offlineFallbackPage))
+      .then((cache) => cache.addAll([
+        offlineFallbackPage,
+        'static/unique.css', // adapte le chemin
+        'static/unique.js',  // adapte le chemin
+        'static/img/cadre/cadre1.jpg', // optionnel
+        'static/img/cadre/cadre2.jpg',  // optionnel
+        'static/img/cadre/cadre3.jpg', // optionnel
+        'static/img/cadre/cadre4.jpg',  // optionnel
+        'static/img/cacher/cacher4.jpg',
+      ]))
   );
 });
 
@@ -29,7 +36,6 @@ self.addEventListener('fetch', (event) => {
     event.respondWith((async () => {
       try {
         const preloadResp = await event.preloadResponse;
-
         if (preloadResp) {
           return preloadResp;
         }
@@ -37,7 +43,6 @@ self.addEventListener('fetch', (event) => {
         const networkResp = await fetch(event.request);
         return networkResp;
       } catch (error) {
-
         const cache = await caches.open(CACHE);
         const cachedResp = await cache.match(offlineFallbackPage);
         return cachedResp;
